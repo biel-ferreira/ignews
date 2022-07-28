c3300725261612aeaa9dec631382b0f3deb9a20c
Gabriel Ferreira <93892548+biel-ferreira@users.noreply.github.com>
2022-07-28 00:19:29 -0300

    configuração da session do next-auth

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,14 @@ import { AppProps } from "next/app";
 import { Header } from "../Components/Header";
 import "../styles/global.scss";
 
+import { SessionProvider } from "next-auth/react";
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <SessionProvider session={pageProps.session}>
       <Header />
       <Component {...pageProps} />
-    </>
+    </SessionProvider>
   );
 }
 
