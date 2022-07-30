a32f769c63a744fd4b56f04515fa8fdcb3e4343d
Gabriel Ferreira <93892548+biel-ferreira@users.noreply.github.com>
2022-07-30 03:58:56 -0300

    subscribeButton

--- src/Components/SubscribeButton/index.tsx
+++ src/Components/SubscribeButton/index.tsx
@@ -1,3 +1,6 @@
+import { useSession, signIn } from "next-auth/react";
+import { api } from "../../services/api";
+import { getStripeJs } from "../../services/stripe-js";
 import styles from "./styles.module.scss";
 
 interface SubscribeButtonProps {
@@ -5,8 +8,33 @@ interface SubscribeButtonProps {
 }
 
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+  const { data } = useSession();
+
+  async function handleSubscribe() {
+    if (!data) {
+      signIn("github");
+      return;
+    }
+
+    try {
+      const response = await api.post("/subscribe");
+
+      const { sessionId } = response.data;
+
+      const stripe = await getStripeJs();
+
+      await stripe.redirectToCheckout({ sessionId: sessionId });
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
   return (
-    <button type="button" className={styles.subscribeButton}>
+    <button
+      type="button"
+      className={styles.subscribeButton}
+      onClick={handleSubscribe}
+    >
       Subscribe now
     </button>
   );
